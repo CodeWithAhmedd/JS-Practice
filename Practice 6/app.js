06b5ae77151591b7edcba58858c783e0727bbe0f
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2024-10-15 20:00:24 +0500

    Update app.js

--- Practice 6/app.js
+++ Practice 6/app.js
@@ -1,23 +1,54 @@
-//FOR LOOP
+// //FOR LOOP
 
-// for (let i = 0; i < 5 ; i++) {
-//     console.log("i")
+// // for (let i = 0; i < 5 ; i++) {
+// //     console.log("i")
+// // }
+
+
+// // let  sum = 0;
+// // let n = prompt("Enter Value")
+// // for (let i = 0; i < n; i++) {
+// //     console.log ("i")}
+
+
+// // let sum = 0;
+// // let n = prompt("Enter Value")
+// // n = Number.parseInt(n)
+// // for (let i = 0; i < n; i++) {
+// //     sum += (i + 1)
+// //     console.log("sum of  first " + i + " natural numbers is " + sum)
+
+// //     }
+
+// // FOR IN LOOP
+// let obj = {
+//     Ahmed : 56,
+//     Rehman : 98,
 // }
+// for (let a in obj){
+//     console.log("Marks Of " + a + " Are " + obj[a])
+// } 
+// // FOR OF LOOP
+// for (let b of "AHMED"){
+//     console.log(b)
+// } 
+// // WHILE LOOP
+// let i = 0;
+// while (i < 20) {
+//     console.log(i + 1)
+//     i++
+// }
+// // DO WHILE LOOP
+// let i = 0;
+// do {
+//     console.log(i + 1)
+    // i++
+//     }while (i < 20)
 
-
-// let  sum = 0;
-// let n = prompt("Enter Value")
-// for (let i = 0; i < n; i++) {
-//     console.log ("i")}
-
-
-// let sum = 0;
-// let n = prompt("Enter Value")
-// n = Number.parseInt(n)
-// for (let i = 0; i < n; i++) {
-//     sum += (i + 1)
-//     console.log("sum of  first " + i + " natural numbers is " + sum)
-
-//     }
-
-// FOR IN LOOP
+let n = prompt("Enter The Value Of N")
+n = Number.parseInt(n)
+let i = 1;
+while(i<=n){
+    console.log(n)
+    i++
+}
