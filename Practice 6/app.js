17c608c7b5b767757e0b223f922b03384cd1e6d2
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2024-10-15 19:20:24 +0500

    Update app.js

---
+++ Practice 6/app.js
@@ -0,0 +1,23 @@
+//FOR LOOP
+
+// for (let i = 0; i < 5 ; i++) {
+//     console.log("i")
+// }
+
+
+// let  sum = 0;
+// let n = prompt("Enter Value")
+// for (let i = 0; i < n; i++) {
+//     console.log ("i")}
+
+
+// let sum = 0;
+// let n = prompt("Enter Value")
+// n = Number.parseInt(n)
+// for (let i = 0; i < n; i++) {
+//     sum += (i + 1)
+//     console.log("sum of  first " + i + " natural numbers is " + sum)
+
+//     }
+
+// FOR IN LOOP
