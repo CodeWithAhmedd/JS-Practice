980acf0265dbb5034e03bb8789804dace5a035b7
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2024-10-15 18:09:54 +0500

    Update app.js

--- Problem 1/app.js
+++ Problem 1/app.js
@@ -1,55 +1,55 @@
-//Question 1
+// //Question 1
 
-let age =prompt("What is your age?")
-if (age>10 && age<20){
-    console.log("You are a teenager");
-    } else {
-        console.log("You are not a teenager");
- }
+// let age =prompt("What is your age?")
+// if (age>10 && age<20){
+//     console.log("You are a teenager");
+//     } else {
+//         console.log("You are not a teenager");
+//  }
 
-//Question 2
+// //Question 2
 
-let a = 1
-let b = 2
-let  c = 3
-let d = 4
-let e = 5
-let sum = a + b + c + d + e
+// let a = 1
+// let b = 2
+// let  c = 3
+// let d = 4
+// let e = 5
+// let sum = a + b + c + d + e
 
-console.log(sum)
+// console.log(sum)
 
-//Question 3
+// //Question 3
 
-//Switch  statement
+// //Switch  statement
 
-let day = prompt("Meeting Schedule for the day:")
+// let day = prompt("Meeting Schedule for the day:")
 
-switch (day) { 
-    case 'Monday': 
-    console.log("Office Meeting")
-    break;
-    case 'Tuesday':
-        console.log("Family Dinner")
-        break;
-        case 'Wednesday':
-            console.log("School Meeting")
-            break;
-            case 'Thursday':
-                console.log("Dentise")
-                break;
-                case 'Friday':
-                    console.log("Meeting With Friends")
-                    break;
-                    case 'Saturday':
-                        console.log("Attend Event")
-                        break;
-                        case 'Sunday':
-                            console.log("Spend Time with Family")
-                            break;
-                            default:
-                                console.log("Invalid Day. First Word Should Be Capital")
-                                break;
-}
+// switch (day) { 
+//     case 'Monday': 
+//     console.log("Office Meeting")
+//     break;
+//     case 'Tuesday':
+//         console.log("Family Dinner")
+//         break;
+//         case 'Wednesday':
+//             console.log("School Meeting")
+//             break;
+//             case 'Thursday':
+//                 console.log("Dentise")
+//                 break;
+//                 case 'Friday':
+//                     console.log("Meeting With Friends")
+//                     break;
+//                     case 'Saturday':
+//                         console.log("Attend Event")
+//                         break;
+//                         case 'Sunday':
+//                             console.log("Spend Time with Family")
+//                             break;
+//                             default:
+//                                 console.log("Invalid Day. First Word Should Be Capital")
+//                                 break;
+// }
 
 
 // //Question 4
@@ -71,11 +71,17 @@ switch (day) {
 
 // Question 5
 
-let num = prompt("What Is Your Age?")
-num = Number.parseInt(num)
-if(num % 2 == 0 && num % 3 == 0 ){
-    console.log("Your number is divible by 2 and 3")
-}
-else{
-    console.log("Your number is not divible by 2 and 3 ")
-}
+// let num = prompt("What Is Your Age?")
+// num = Number.parseInt(num)
+// if(num % 2 == 0 && num % 3 == 0 ){
+//     console.log("Your number is divible by 2 and 3")
+// }
+// else{
+//     console.log("Your number is not divible by 2 and 3 ")
+// }
+
+// Question 6
+
+let age = 4
+a = age > 18? "You can drive"  : "You can't drive"
+console.log(a)
