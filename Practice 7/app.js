805a9626ed2a4350184f20c13d9cb19ca0d21d3b
Muhammad Ahmed <muhammadahmedyt@gmail.com>
2024-10-17 16:00:02 +0500

    Update app.js

---
+++ Practice 7/app.js
@@ -0,0 +1,21 @@
+let a = 5
+let b = 4
+let c = 3
+
+// console.log("Average  of a, b, c is: ", 1 + (a + b + c) / 3);
+
+function  lol(a, b, c) {
+    console.log("Hahaha")
+return 1 + (a + b + c) / 3
+}
+console.log (lol(a, b, c))
+console.log (lol(b ,  c , a))
+console.log (lol(c , a , b))
+
+
+const hello =()=>{
+    console.log("lol hahahahahaha")
+    return "hi"
+}
+let v = hello();
+console.log(v)
